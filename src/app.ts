1dbee244610a97bb3243c710765c11c2eb5da26c
Arvin619 <wasd572@gmail.com>
2022-07-06 07:27:09 +0800

    add env file and config

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app: Application = express()
 
-const port: number = 5000
+const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello');
